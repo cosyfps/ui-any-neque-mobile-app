8a7825c50e20f1ab8332a305c6ba60603db1e21c
cosyfps <kelvin.morenog28@gmail.com>
2026-09-24 20:50:04 -0300

    perf(build): retira Ionic y baja el bundle a 259 kB

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { appConfig } from './app.config';
 
 describe('AppComponent', () => {
-  it('monta el shell de Ionic', () => {
+  it('monta el outlet de la app', () => {
     TestBed.configureTestingModule({
       imports: [AppComponent],
       providers: [provideRouter([])],
@@ -19,8 +19,8 @@ describe('AppComponent', () => {
     expect(fixture.nativeElement.querySelector('router-outlet')).toBeTruthy();
   });
 
-  it('appConfig registra router, Ionic y los adapters de datos', () => {
-    expect(appConfig.providers.length).toBeGreaterThanOrEqual(3);
+  it('appConfig registra el router y los adapters de datos', () => {
+    expect(appConfig.providers.length).toBeGreaterThanOrEqual(2);
   });
 
   it('appConfig resuelve el puerto CLOCK', () => {
